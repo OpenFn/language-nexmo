ec8e51fe9e676502971d1e0e47c70682cd0951ef
Santiago Adolfo Gutierrez Akel <sgutierrezakel@gmail.com>
2018-01-19 17:41:34 -0300

    adds a test for sendSMS

--- test/index.js
+++ test/index.js
@@ -1,67 +1,27 @@
 import {expect} from 'chai';
-
-import nock from 'nock';
-import ClientFixtures, {fixtures} from './ClientFixtures'
-
 import Adaptor from '../src';
-const {execute, post} = Adaptor;
+const {execute, sendSMS} = Adaptor;
 
 describe("execute", () => {
 
-  it("executes each operation in sequence", (done) => {
-    let state = {}
-    let operations = [
-      (state) => {
-        return {counter: 1}
+  it("sends an SMS and expects a delivery status", (done) => {
+    let state = {
+      configuration: {
+        "apiKey": "613118f9",
+        "apiSecret": "b7e9d82697feb58b"
       },
-      (state) => {
-        return {counter: 2}
-      },
-      (state) => {
-        return {counter: 3}
-      }
-    ]
+      data: {}
+    }
 
-    execute(...operations)(state).then((finalState) => {
-      expect(finalState).to.eql({counter: 3})
+    const from = "OpenFn"
+    const to = "56979395234"
+    const message = "Hello World!"
+
+    execute(sendSMS(from, to, message))(state)
+    .then((finalState) => {
+      expect(finalState.data.messages[0].status).to.eql("0")
     }).then(done).catch(done)
 
   })
 
-  it("assigns references, data to the initialState", () => {
-    let state = {}
-
-    let finalState = execute()(state)
-
-    execute()(state).then((finalState) => {
-      expect(finalState).to.eql({references: [], data: null})
-    })
-
-  })
-})
-
-describe("post", () => {
-
-  before(() => {
-    nock('https://fake.server.com').post('/api').reply(200, {foo: 'bar'});
-  })
-
-  it("calls the callback", () => {
-    let state = {
-      configuration: {
-        username: "hello",
-        password: "there"
-      }
-    };
-
-    return execute(post({
-      "url": "https://fake.server.com/api",
-      "headers": null,
-      "body": {"a": 1}
-    }))(state).then((state) => {
-      let responseBody = state.response.body
-      // Check that the post made it's way to the request as a string.
-      expect(responseBody).to.eql({foo: 'bar'})
-    })
-  })
 })
